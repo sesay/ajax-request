40ec2be22641d18eeb389db2ba62b9b4c8d3391a
sesay umar <sesay umar>
2016-04-27 12:17:10 +0400

    navigation and search field done

---
+++ gruntfile.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = function(grunt) {
+  //load plugins
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+
+  //compress JS files
+  grunt.initConfig({
+    uglify: {
+      my_target: {
+        files: {
+          '_/js/scripts.js' : ['_/components/js/scripts.js']
+        } // files
+      } //my_target
+    }, // uglify
+    compass: {
+      dev: {
+        options: {
+          config: 'config.rb'
+        }
+      }
+    }, // compass
+    watch: {
+      options: { livereload: true},
+      scripts: {
+        files: ['_/components/js/*.js'],
+        tasks:['uglify']
+      }, //scripts
+      sass: {
+        files: ['_/components/sass/*.scss'],
+        tasks: ['compass:dev']
+      },
+      html: {
+        files: ['*.html']
+      }
+    } // watch
+
+  }) //initConfig
+  grunt.registerTask('default','watch');
+} //exports
